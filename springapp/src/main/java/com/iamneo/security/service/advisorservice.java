ed2140993b4eebb027e7dd800366810fadcf1d64
dharshinir2 <727721euai017@skcet.ac.in>
2023-07-24 09:42:20 +0000

    commit

---
+++ springapp/src/main/java/com/iamneo/security/service/advisorservice.java
@@ -0,0 +1,18 @@
+//package com.iamneo.security.service;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.iamneo.security.entity.advisoraccount;
+//import com.iamneo.security.repository.advisorRepository;
+//@Service
+//public class advisorservice {
+//	@Autowired
+//	advisorRepository repository;
+//	public Optional<advisoraccount>getStudent(String mailid){
+//		return repository.findById(mailid);
+//	}
+//
+//}
